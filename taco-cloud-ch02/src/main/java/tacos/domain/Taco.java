e2dea6899bc8927b1e70ddb087bb8149950b0600
Lin I-Ming <soakpenny@hotmail.com>
2021-01-26 00:09:24 +0800

    first commit on chapter 02 on spring in action 5th

---
+++ taco-cloud-ch02/src/main/java/tacos/domain/Taco.java
@@ -0,0 +1,21 @@
+package tacos.domain;
+
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+public class Taco {
+
+	@NotNull
+	@Size(min = 5, message = "Name must be at least 5 characters long")
+	private String name;
+	
+	@NotNull
+	@Size(min = 1, message = "Tou must choose at least 1 ingredient")
+	private List<String> ingredients;
+	
+}
