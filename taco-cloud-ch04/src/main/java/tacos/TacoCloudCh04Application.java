a1ab7b0b4a5b1a50e219211a531ab73d573ccd0e
Lin I-Ming <soakpenny@hotmail.com>
2021-02-01 00:59:25 +0800

    2nd commit on chapter 04 on spring in action 5th

---
+++ taco-cloud-ch04/src/main/java/tacos/TacoCloudCh04Application.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudCh04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudCh04Application.class, args);
+	}
+
+}
